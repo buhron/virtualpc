5532b75ac062a750402320d2562fdbc775c27d75
buhron <103539649+buhron@users.noreply.github.com>
2022-05-14 16:17:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup
 
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
-    name='Measurements',
-    url='https://github.com/jladan/package_demo',
-    author='John Ladan',
-    author_email='jladan@uwaterloo.ca',
+    name='Virtual PC',
+    url='https://github.com/buhron/virtualpc',
+    author='buhron',
+    author_email='mbh90180@gmail.com',
     # Needed to actually package something
-    packages=['measure'],
+    packages=['virtual-env-pc'],
     # Needed for dependencies
     install_requires=['numpy'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
     license='MIT',
-    description='An example of a python package from pre-existing code',
+    description='Run Virtual PC on python!',
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
 )
